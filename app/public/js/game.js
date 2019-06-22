fa235497d704cdd611a332d24c7a5ab8e82840b0
vinicius <viniciussouzav@gmail.com>
2019-06-22 15:01:18 -0300

    finalizando a dinâmica do jogo

--- app/public/js/game.js
+++ app/public/js/game.js
@@ -6,7 +6,7 @@
 import Carta from './Carta.js';
 import Baralho from './Baralho.js';
 
-var objCards = null;
+var objBar = null;
 
 $(document).ready(() => initGame())
 
@@ -16,37 +16,68 @@ async function initGame() {
     const cardKing = new Carta('king', 'image/king.png');
     const cardKing2 = new Carta('king2', 'image/king.png');
 
-    objCards = new Baralho();
+    objBar = new Baralho();
 
-    objCards.inserirCarta(cardQueen);
-    objCards.inserirCarta(cardKing);
-    objCards.inserirCarta(cardKing2);
+    objBar.inserirCarta(cardQueen);
+    objBar.inserirCarta(cardKing);
+    objBar.inserirCarta(cardKing2);
 
-    showCards(await shuffle(objCards));
+    await shuffleQuenn()
+    showCards();
 }
 
-//função para embaralhar o vetor de cartas
-function shuffle(objBar) {
+//função para embaralhar rainha
+function shuffleQuenn() {
     let pos = Math.floor(Math.random() * 3);
     let aux = objBar.vetBar[pos];
     
     objBar.vetBar[pos] = objBar.vetBar[0];
     objBar.vetBar[0] = aux;
-    return objBar;
-}
 
-//função para exibir cartas na tela
-function showCards(objBar) {
     let imgs = $('img');
 
     for (let i = 0; i < objBar.vetBar.length; i++) {
         imgs[i*2].src = objBar.vetBar[i].caminho;
+        imgs[i*2].id = objBar.vetBar[i].tipo;
         imgs[i*2+1].src = 'image/card.png';
+        imgs[i*2+1].id = objBar.vetBar[i].tipo;
+    }
+}
+
+//função para embaralhar o vetor de cartas
+function shuffle() {
+    
+    for (let i = 0; i < 25; i++) {
+        let pos = Math.floor(Math.random() * 3);
+        let pos2 = Math.floor(Math.random() * 3);
+        let aux = objBar.vetBar[pos];
+        
+        objBar.vetBar[pos] = objBar.vetBar[pos2];
+        objBar.vetBar[pos2] = aux;
     }
 
+    let imgs = $('img');
 
-    
-
-    setTimeout(() =>  $('.card').trigger( "click" ).flip(), 3000);
+    for (let i = 0; i < objBar.vetBar.length; i++) {
+        imgs[i*2].src = objBar.vetBar[i].caminho;
+        imgs[i*2].id = objBar.vetBar[i].tipo;
+        imgs[i*2+1].src = 'image/card.png';
+        imgs[i*2+1].id = objBar.vetBar[i].tipo;
+    }
 }
+
+//função para exibir cartas na tela
+function showCards() {
+    setTimeout(() =>  $('.card').trigger( 'click' ).flip(), 1000);
+}
+
 $('.card').flip();
+
+$('.card img').click( async function() {
+    await shuffle();
+
+    if(this.id != 'queen') 
+        alert('Haahahahhahahahahahah');
+
+    setTimeout(() =>  window.location.href = '/', 1000);
+});
